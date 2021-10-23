c73372254e8355a8980f96bed0760a6f22c7e98a
OverGeek <drv.agwl@gmail.com>
2021-10-23 23:02:23 +0530

    added randomSolve, gridSolve, getDensity, and other functions to GridSolver

--- engine/GridSolver.py
+++ engine/GridSolver.py
@@ -4,20 +4,38 @@ import phyre
 import os.path as osp
 import numpy as np
 from tqdm import tqdm
+from math import sqrt
+import random
 
 
 class Line:
-    def __init__(self, start=(0, 0), end=(0, 0), orientation="H"):
+    def __init__(self, start=(0, 0), end=(0, 0), orientation="H", obj_idx=None, line_no=0):
         """"
         Line: ax + by = c
         """
-        self.start = start
-        self.end = end
+        self.start = (round(start[0]), round(start[1]))
+        self.end = (round(end[0]), round(end[1]))
         self.orientation = orientation
 
-        self.a = end[1] - start[1]
-        self.b = start[0] - end[0]
-        self.c = start[0] * (end[1] - start[1]) + start[1] * (start[0] - end[0])
+        self.a = self.end[1] - self.start[1]
+        self.b = self.start[0] - self.end[0]
+        self.c = self.start[0] * (self.end[1] - self.start[1]) + self.start[1] * (self.start[0] - self.end[0])
+
+        self.obj_idx = obj_idx  # obj_idx to which the line belongs
+        self.line_no = line_no
+
+        # make c positive
+        if self.c < 0:
+            self.c *= -1
+            self.a *= -1
+            self.b *= -1
+
+        # check c=0
+        if self.c == 0:
+            if self.a != 0:
+                self.a = 1
+            elif self.b != 0:
+                self.b = 1
 
     def setStart(self, start):
         self.start = start
@@ -36,6 +54,15 @@ class Line:
 
         val = self.a * point[0] + self.b * point[1]
         return val >= self.c
+
+    def getDistFromPoint(self, point):
+        x = point[0]
+        y = point[1]
+
+        numerator = abs(self.a * x + self.b * y - self.c)
+        denominator = sqrt(self.a ** 2 + self.b ** 2) + 1e-6
+
+        return numerator / denominator
 
 
 class GridSolver:
@@ -58,7 +85,15 @@ class GridSolver:
         self.objectFeatures = self.featurized_objects.features[0]
         self.num_objects = self.objectFeatures.shape[0]
         self.num_lines = 6
-        self.regions = [[Line() for j in range(self.num_lines)] for i in range(self.num_objects)]
+        self.width = 256
+        self.height = 256
+
+        self.lineDatabase = [[Line() for j in range(self.num_lines)] for i in range(self.num_objects)]
+        self.allLines = [Line(start=(0, 0), end=(0, 255), orientation="V", obj_idx="L"),
+                         Line(start=(255, 0), end=(255, 255), orientation="V", obj_idx="R"),
+                         Line(start=(0, 0), end=(255, 0), orientation="H", obj_idx="U"),
+                         Line(start=(0, 255), end=(255, 255), orientation="H", obj_idx="D")]
+        self.regions = {}
 
     def getSplitPoints(self):
         """
@@ -104,14 +139,295 @@ class GridSolver:
     def makeRegions(self):
         splitPoints = self.getSplitPoints()
         for i in range(self.num_objects):
-            l1 = Line(start=(splitPoints[i][1][0], 0), end=(splitPoints[i][1][0], 255.), orientation="V")
-            l2 = Line(start=(splitPoints[i][0][0], 0), end=(splitPoints[i][0][0], 255.), orientation="V")
-            l3 = Line(start=(splitPoints[i][3][0], 0), end=(splitPoints[i][3][0], 255.), orientation="V")
-            l4 = Line(start=(0, splitPoints[i][2][1]), end=(255., splitPoints[i][2][1]), orientation="H")
-            l5 = Line(start=(0, splitPoints[i][0][1]), end=(255., splitPoints[i][0][1]), orientation="H")
-            l6 = Line(start=(0, splitPoints[i][4][1]), end=(255., splitPoints[i][4][1]), orientation="H")
+            l1 = Line(start=(splitPoints[i][1][0], 0), end=(splitPoints[i][1][0], 255.), orientation="V", obj_idx=i,
+                      line_no=0)
+            l2 = Line(start=(splitPoints[i][0][0], 0), end=(splitPoints[i][0][0], 255.), orientation="V", obj_idx=i,
+                      line_no=1)
+            l3 = Line(start=(splitPoints[i][3][0], 0), end=(splitPoints[i][3][0], 255.), orientation="V", obj_idx=i,
+                      line_no=2)
+            l4 = Line(start=(0, splitPoints[i][2][1]), end=(255., splitPoints[i][2][1]), orientation="H", obj_idx=i,
+                      line_no=3)
+            l5 = Line(start=(0, splitPoints[i][0][1]), end=(255., splitPoints[i][0][1]), orientation="H", obj_idx=i,
+                      line_no=4)
+            l6 = Line(start=(0, splitPoints[i][4][1]), end=(255., splitPoints[i][4][1]), orientation="H", obj_idx=i,
+                      line_no=5)
 
-            self.regions[i] = [l1, l2, l3, l4, l5, l6]
+            self.lineDatabase[i] = [l1, l2, l3, l4, l5, l6]
+
+        for i in range(self.num_objects):
+            for line in self.lineDatabase[i]:
+                self.allLines.append(line)
+
+        pbar = tqdm(total=self.width * self.height)
+        for x in range(self.width):
+            for y in range(self.height):
+                point = (x, y)
+                # finding the bounding lines
+                minDistLineL = self.allLines[0]
+                minDistLineR = self.allLines[0]
+                minDistLineU = self.allLines[0]
+                minDistLineD = self.allLines[0]
+                minDistL = 1e6
+                minDistR = 1e6
+                minDistU = 1e6
+                minDistD = 1e6
+
+                for line in self.allLines:
+                    if line.orientation == "V" and line.locatePoint(point) and line.getDistFromPoint(point) < minDistL:
+                        minDistLineL = line
+                        minDistL = line.getDistFromPoint(point)
+
+                    if line.orientation == "V" and not line.locatePoint(point) and line.getDistFromPoint(
+                            point) < minDistR:
+                        minDistLineR = line
+                        minDistR = line.getDistFromPoint(point)
+
+                    if line.orientation == "H" and line.locatePoint(point) and line.getDistFromPoint(
+                            point) < minDistU:
+                        minDistLineU = line
+                        minDistU = line.getDistFromPoint(point)
+
+                    if line.orientation == "H" and not line.locatePoint(point) and line.getDistFromPoint(
+                            point) < minDistD:
+                        minDistLineD = line
+                        minDistD = line.getDistFromPoint(point)
+
+                regionName = f"{minDistLineL.obj_idx}{minDistLineL.line_no}:{minDistLineR.obj_idx}{minDistLineR.line_no}:{minDistLineU.obj_idx}{minDistLineU.line_no}:{minDistLineD.obj_idx}{minDistLineD.line_no}"
+
+                if regionName not in self.regions.keys():
+                    self.regions[regionName] = []
+
+                self.regions[regionName].append(point)
+
+                pbar.update(1)
+
+    def getRegionForPointWrtObject(self, point, obj_idx):
+        region_marker = np.array([0 for i in range(16)])  # 1 if point belongs to region with region id = array idx.
+        # 0 otherwise
+        lines = self.lineDatabase[obj_idx]
+
+        if lines[0].locatePoint(point) and not lines[1].locatePoint(point) \
+                and lines[3].locatePoint(point) and not lines[4].locatePoint(point):
+            region_marker[0] = 1
+
+        if lines[1].locatePoint(point) and not lines[2].locatePoint(point) \
+                and lines[3].locatePoint(point) and not lines[4].locatePoint(point):
+            region_marker[1] = 1
+
+        if lines[0].locatePoint(point) and not lines[1].locatePoint(point) \
+                and lines[4].locatePoint(point) and not lines[5].locatePoint(point):
+            region_marker[2] = 1
+
+        if lines[1].locatePoint(point) and not lines[2].locatePoint(point) \
+                and lines[4].locatePoint(point) and not lines[5].locatePoint(point):
+            region_marker[3] = 1
+
+        if not lines[0].locatePoint(point) \
+                and lines[3].locatePoint(point) and not lines[4].locatePoint(point):
+            region_marker[4] = 1
+
+        if lines[2].locatePoint(point) \
+                and lines[3].locatePoint(point) and not lines[4].locatePoint(point):
+            region_marker[5] = 1
+
+        if not lines[0].locatePoint(point) \
+                and lines[4].locatePoint(point) and not lines[5].locatePoint(point):
+            region_marker[6] = 1
+
+        if lines[2].locatePoint(point) \
+                and lines[4].locatePoint(point) and not lines[5].locatePoint(point):
+            region_marker[7] = 1
+
+        if lines[0].locatePoint(point) and not lines[1].locatePoint(point) \
+                and not lines[3].locatePoint(point):
+            region_marker[8] = 1
+
+        if lines[1].locatePoint(point) and not lines[2].locatePoint(point) \
+                and not lines[3].locatePoint(point):
+            region_marker[9] = 1
+
+        if lines[0].locatePoint(point) and not lines[1].locatePoint(point) \
+                and lines[5].locatePoint(point):
+            region_marker[10] = 1
+
+        if lines[1].locatePoint(point) and not lines[2].locatePoint(point) \
+                and lines[5].locatePoint(point):
+            region_marker[11] = 1
+
+        if not lines[0].locatePoint(point) and not lines[3].locatePoint(point):
+            region_marker[12] = 1
+
+        if lines[2].locatePoint(point) and not lines[3].locatePoint(point):
+            region_marker[13] = 1
+
+        if not lines[0].locatePoint(point) and lines[5].locatePoint(point):
+            region_marker[14] = 1
+
+        if lines[2].locatePoint(point) and lines[5].locatePoint(point):
+            region_marker[15] = 1
+
+        return region_marker
+
+    @staticmethod
+    def makeLineOnGrid(grid, line):
+        x1, x2 = line.start[0], line.end[0]
+        y1, y2 = line.start[1], line.end[1]
+
+        for y in range(y1, y2 + 1):
+            for x in range(x1, x2 + 1):
+                grid[y, x, :] = np.array([255., 0., 0.])
+
+        return grid
+
+    def makeGridForObject(self, grid, obj_idxs, visualise=False):
+        if len(self.allLines) == 0:
+            self.makeRegions()
+
+        for obj_idx in obj_idxs:
+            for line in self.lineDatabase[obj_idx]:
+                grid = self.makeLineOnGrid(grid, line)
+
+        if visualise:
+            plt.imshow(grid)
+            plt.show()
+
+        return grid
+
+    def visualiseRegions(self):
+        # check if regions have been made
+        if len(self.allLines) == 0:
+            print("Call makeRegions first")
+            return
+
+        grid = np.zeros((256, 256, 3))
+        for region_name, region in self.regions.items():
+            color = np.random.randint(0, 255, (3,)) / 255.
+            for point in region:
+                grid[point[1], point[0], :] = color
+
+        plt.imshow(grid)
+        plt.show()
+
+    def randomSolve(self, sim, task_idx, task, visualise=False):
+        solution_points = []
+        initial_scene = phyre.observations_to_float_rgb(sim.initial_scenes[task_idx])
+        cache = phyre.get_default_100k_cache('ball')
+        actions = cache.action_array
+
+        cache_list = actions[cache.load_simulation_states(task) == 1]
+
+        solved = 0
+        tries = 0
+        max_tries = 1000
+        num_solving = 100
+        while solved < num_solving:
+            tries += 1
+            actionlist = cache_list
+
+            if len(actionlist) == 0:
+                print("WARNING no solution action in cache at task", task)
+                actionlist = [np.random.rand(3)]
+
+            action = random.choice(actionlist)
+            res = sim.simulate_action(task_idx, action, need_featurized_objects=True, stride=1)
+
+            initial_scene = self.makeGridForObject(initial_scene, obj_idxs=[0, 1, 2], visualise=False)
+
+            try:
+                features = res.featurized_objects.features
+            except:
+                if tries > max_tries:
+                    break
+                else:
+                    continue
+
+            if res.status.is_solved() and not res.status.is_invalid():
+                tries = 0
+                solved += 1
+
+                x, y = res.featurized_objects.features[0][-1][0] * 255., \
+                       (1 - res.featurized_objects.features[0][-1][1]) * 255.
+                x, y = round(x), round(y)
+
+                initial_scene[y, x, :] = [1., 0., 0.]
+
+                solution_points.append((x, y))
+
+        if visualise:
+            plt.imshow(initial_scene)
+            plt.show()
+
+        return solution_points
+
+    def getRegionDensities(self, solution_points):
+        if len(self.allLines) == 0:
+            self.makeRegions()
+
+        region_density = {}
+
+        most_dense_region = list(self.regions.keys())[0]
+        max_density = 0
+        for region_name, region in self.regions.items():
+            num_solving_points = 0
+            total_points = len(region)
+
+            for point in solution_points:
+                if point in region:
+                    num_solving_points += 1
+
+            density = num_solving_points / total_points
+            region_density[region_name] = density
+
+            if density > max_density:
+                most_dense_region = region_name
+                max_density = density
+
+        return region_density, most_dense_region
+
+    def gridSolve(self, sim, task_idx, task, proposed_region, visualise=False):
+        if proposed_region not in self.regions.keys():
+            print("Proposed region not found in the grid")
+            return False
+
+        region_space = self.regions[proposed_region]
+        initial_scene = phyre.observations_to_float_rgb(sim.initial_scenes[task_idx])
+        initial_scene = self.makeGridForObject(initial_scene, obj_idxs=[0, 1, 2], visualise=True)
+
+        cache = phyre.get_default_100k_cache('ball')
+        actions = cache.action_array
+        actionList = actions[cache.load_simulation_states(task) == 1]
+        if len(actionList) == 0:
+            print("WARNING no solution action in cache at task", task)
+            actionList = [np.random.rand(3)]
+
+        solved = 0
+        tries = 0
+        max_tries = 1000
+        num_solving = 100
+        while solved < num_solving:
+            tries += 1
+
+            point = random.choice(region_space)  # choose random point from proposed region
+            radius = random.choice(actionList)[-1]  # choose a random radius from cache
+            action = np.array([point[0] / 255., 1. - point[1] / 255., radius])
+
+            try:
+                res = sim.simulate_action(task_idx, action, need_featurized_objects=True, stride=1)
+            except:
+                if tries > max_tries:
+                    break
+                else:
+                    continue
+
+            if res.status.is_solved() and not res.status.is_invalid():
+                tries = 0
+                solved += 1
+
+        if visualise:
+            plt.imshow(initial_scene)
+            plt.show()
+
+        return solved / num_solving
 
 
 if __name__ == '__main__':
@@ -128,75 +444,13 @@ if __name__ == '__main__':
     sim = phyre.initialize_simulator(tasks_ids, 'ball')
 
     gridSolver = GridSolver(sim.initial_featurized_objects[0])
-    print(gridSolver.getSplitPoints().shape)
     gridSolver.makeRegions()
+    gridSolver.makeGridForObject(phyre.observations_to_float_rgb(sim.initial_scenes[0]), obj_idxs=[0, 1, 2],
+                                 visualise=True)
+    gridSolver.visualiseRegions()
+    solution_points = gridSolver.randomSolve(sim, task_idx=0, task=tasks_ids[0], visualise=True)
+    _, solution_region = gridSolver.getRegionDensities(solution_points)
 
-#     pbar = tqdm(len(tasks_ids))
-#     for task_idx, task in enumerate(tasks_ids):
-#         cache = phyre.get_default_100k_cache('ball')
-#         actions = cache.action_array
-#
-#         cache_list = actions[cache.load_simulation_states(task) == 1]
-#
-#         solved = 0
-#         tries = 0
-#         max_tries = 1000
-#         num_solving = 100
-#         while solved < num_solving:
-#             tries += 1
-#             actionlist = cache_list
-#
-#             if len(actionlist) == 0:
-#                 print("WARNING no solution action in cache at task", task)
-#                 actionlist = [np.random.rand(3)]
-#
-#             action = random.choice(actionlist)
-#             res = sim.simulate_action(task_idx, action,
-#                                       need_featurized_objects=True, stride=1)
-#
-#             # make split lines
-#             body_list = res.body_list
-#             green_idx = body_list.index('GreenObject')
-#             black_idx = body_list.index('BlackObject')
-#             goal_idx = body_list.index('GoalObject')
-#             #
-#             # obj_centers = [(x, y) for x, y in zip(res.featurized_objects.features[0, :, 0],
-#             #                                       res.featurized_objects.features[0, :, 1])]
-#             # obj_centers = [(round(x * 255.), round((1. - y) * 255.)) for x, y in obj_centers]
-#             #
-#             # splitLines = [getSplitLines(c[0], c[1]) for c in obj_centers[:-1]]
-#             #
-#             # for obj_line in splitLines:
-#             #     for line in obj_line:
-#             #         for coord in line:
-#             #             solution_maps[0][coord[1], coord[0]] = 5
-#
-#             try:
-#                 features = res.featurized_objects.features
-#             except:
-#                 if tries > max_tries:
-#                     break
-#                 else:
-#                     continue
-#
-#             if res.status.is_solved() and not res.status.is_invalid():
-#                 tries = 0
-#                 solved += 1
-#
-#                 # images = [phyre.observations_to_uint8_rgb(x) for x in res.images]
-#                 x, y = res.featurized_objects.features[0][-1][0] * 255., \
-#                        (1 - res.featurized_objects.features[0][-1][1]) * 255.
-#                 x, y = round(x), round(y)
-#
-#                 obj_channels = get_obj_channels(np.array([res.images[0]]))[0]
-#                 # images[0][y, x, :2] += 1
-#                 solution_maps[task_idx][:, :, 2] = (obj_channels[3] + obj_channels[5])
-#                 solution_maps[task_idx][:, :, 1] = obj_channels[1]
-#                 solution_maps[task_idx][y, x, 0] += 1.
-#
-#         pbar.update(1)
-#
-#         for i, img in enumerate(solution_maps):
-#             plt.imsave(f"{i+1}.png", solution_maps[i])
-#
-# print("")
+    gridSolver2 = GridSolver(sim.initial_featurized_objects[1])
+    gridSolver2.makeRegions()
+    solved = gridSolver2.gridSolve(sim, 1, tasks_ids[1], solution_region, visualise=False)
